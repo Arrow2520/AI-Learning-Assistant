ef5b342405efe5d293c6c9776a188683a59579c5
Arrow2520 <soubhikrahac@gmail.com>
2025-10-31 15:34:09 +0530

    Fixed Summary Error (2)

--- app.py
+++ app.py
@@ -272,7 +272,7 @@ def summarize_pdf(doc_id):
         return render_template('summary.html', summary_html=summary_html)
     except Exception as e:
         error_message = f"An error occurred with the AI model: {e}"
-        return render_template('summary.html', summary_text=error_message)
+        return render_template('summary.html', summary_html=error_message)
 
 
 @app.route('/generate_flashcards/<string:doc_id>', methods=['GET'])
